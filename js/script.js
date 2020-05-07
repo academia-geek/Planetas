ea53ea7c14c0c1927391eb3c34930860f264501b
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-07 10:35:36 -0500

    cambios

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var enviarEvaluacion = (e) => {
+    [document.querySelectorAll(".numeracion_planetas")].map((input) => {
+        console.log(input);
+    })
+}
+
+	
+document.querySelector("#btn_enviar_evaluacion").addEventListener("click", enviarEvaluacion);
+
+ 
